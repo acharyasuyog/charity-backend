706a70746c4ec97de4a5ccfcd0a7c58ff8d65421
acharyasuyog <suyogacharya50@gmail.com>
2024-12-20 09:11:25 +0545

    donation comment added

--- routes/donation.routes.js
+++ routes/donation.routes.js
@@ -3,5 +3,6 @@ import { createDonation } from '../controllers/donation.controller.js';
 import verifyToken from '../middlewares/auth.middleware.js';
 const router = express.Router();
 router.post('/', verifyToken, createDonation);
+//this is the router for donation
 
 export default router;
