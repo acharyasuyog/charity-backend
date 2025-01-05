d3181e2c12ef3ed42fa2d7eea7aedc7228dae02d
acharyasuyog <suyogacharya50@gmail.com>
2025-01-05 22:48:47 +0545

    fix: added the routes for category

--- routes/category.route.js
+++ routes/category.route.js
@@ -4,11 +4,14 @@ import {
   getCategories,
   getCategory,
   updateCategory,
+  deleteCategory,
 } from '../controllers/category.controller.js';
 const router = express.Router();
 
 router.post('/', createCategory);
 router.get('/', getCategories);
 router.get('/:id', getCategory);
+router.put('/:id', updateCategory);
+router.delete('/:id', deleteCategory);
 
 export default router;
