e87663efdbb466f273fbbf3e594d9a961cf9a000
acharyasuyog <suyogacharya50@gmail.com>
2025-03-18 06:16:36 +0545

    fix: cors error

--- backend/index.js
+++ backend/index.js
@@ -4,24 +4,37 @@ import express from 'express';
 import connectDB from './config/database.config.js';
 import mainRouter from './routes/index.route.js';
 import swaggerUi from 'swagger-ui-express';
-import swaggerJsdoc from 'swagger-jsdoc';
 import YAML from 'yamljs';
+import cors from 'cors';
+
 const swaggerDocument = YAML.load('./swagger.yaml');
 
-connectDB();
-import cors from 'cors'; /// for testing only
 const app = express();
-app.use(express.json());
-app.use('/api/v1', mainRouter);
-app.use(cors()); // Allow all origins
 
+// Connect to DB
+connectDB();
+
+app.use(cors());
+
+app.use(express.json());
+
+// Main routes
+app.use('/api/v1', mainRouter);
+
+// Swagger Documentation Route
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Home Route
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-const PORT = process.env.PORT || 5000;
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ message: 'Something went wrong' });
+});
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port http://localhost:${PORT}`);
 });
